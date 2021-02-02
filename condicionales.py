36dc5eb1d27272a01d3880d51a84cd35a03add16
Cesar <30559667+CesarAroon@users.noreply.github.com>
2021-02-02 15:55:26 -0500

    Add files via upload

---
+++ condicionales.py
@@ -0,0 +1,15 @@
+# edad=int(input("Escribe tu edad: "))
+# if edad>17:
+#     print("Eres mayor de edad")
+# else:
+#     print("Eres menor de esdad")
+
+numero=int(input("Escribe un numero: "))
+if numero>5:
+    print("es mayor a 5")
+elif numero==5:
+    print("es igual a 5")
+else:
+    print("es menor que 5")
+    
+    
